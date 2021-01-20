68861c17a265962a04572a1c4c554bbbe7b14fc4
Oscar Manzano <oscar.oscarmanzano@gmail.com>
2021-01-20 13:28:19 +0100

    JS files added
    3 new JS files added

---
+++ async-await.js
@@ -0,0 +1,15 @@
+async function requestHandler (req,res) {
+
+    try { 
+        const user = await User.findById(req.userId);
+        const tasks = await Tasks.findById(user.tasksId);
+        tasks.completed = true;
+        await tasks.save();
+        res.send('Tasks Completed');  
+        // comment
+        // comment
+    }
+    catch(e){
+        res.send(e);
+    }
+}
